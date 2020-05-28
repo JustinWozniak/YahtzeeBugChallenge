07efb6e7589e622efe0c46c455e0a039ad515f59
Justin Wozniak <wozzytheprogrammer@gmail.com>
2020-05-28 11:10:03 -0700

    Fixed full house class bug

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# YahtzeeBugChallenge" 
+
+Starter code complete with piles of bugs...If i can fix all the bugs, Ill have a working yahtzee app!
--- src/Rules.js
+++ src/Rules.js
@@ -56,15 +56,23 @@ class SumDistro extends Rule {
 }
 
 /** Check if full house (3-of-kind and 2-of-kind) */
+class FullHouse extends Rule {
+  evalRoll = dice => {
+    const freqs = this.freq(dice)
+    console.log(freqs)
 
-class FullHouse {
-  // TODO
+    return (freqs.includes(2) || freqs.includes(3)) ? 25 : 0
+  };
 }
 
 /** Check for small straights. */
+class SmallStraight extends Rule {
+  evalRoll = dice => {
+    const d = new Set(dice);
 
-class SmallStraight {
-  // TODO
+    // large straight must be 5 different dice & only one can be a 1 or a 6
+    return d.size === 5 && (!d.has(1) || !d.has(6)) ? this.score : 0;
+  };
 }
 
 /** Check for large straights. */
@@ -100,7 +108,7 @@ const threeOfKind = new SumDistro({ count: 3 });
 const fourOfKind = new SumDistro({ count: 4 });
 
 // full house scores as flat 25
-const fullHouse = "TODO";
+const fullHouse = new FullHouse({ score: 25 });
 
 // small/large straights score as 30/40
 const smallStraight = "TODO";
